f7d05ff0666e52a8c944d37ac864f1ecd9c3fa74
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-04-20 10:52:10 +0200

    fix: namespaces

--- src/mxlbricks/__init__.py
+++ src/mxlbricks/__init__.py
@@ -12,9 +12,9 @@ __all__ = [
 ]
 
 from . import names
+from ._e26 import get_ebeling_2026
+from ._m16_npq import get_matuszynska2016_npq
 from .models import (
-    get_ebeling_2026,
-    get_matuszynska2016_npq,
     get_matuszynska2016_phd,
     get_matuszynska2019,
     get_poolman2000,
